d968a1db796bb1d00b81239a426f3d2148826a83
DoSer <63659999+DoSerZBlock@users.noreply.github.com>
2026-07-15 10:53:48 +0800

    允許子指令型 Slash Command

--- src/commands/command.ts
+++ src/commands/command.ts
@@ -1,7 +1,12 @@
-import type { ChatInputCommandInteraction, SlashCommandBuilder, SlashCommandOptionsOnlyBuilder } from 'discord.js';
+import type {
+  ChatInputCommandInteraction,
+  SlashCommandBuilder,
+  SlashCommandOptionsOnlyBuilder,
+  SlashCommandSubcommandsOnlyBuilder
+} from 'discord.js';
 import type { BotContext } from '../types';
 
 export interface CommandDefinition {
-  data: SlashCommandBuilder | SlashCommandOptionsOnlyBuilder;
+  data: SlashCommandBuilder | SlashCommandOptionsOnlyBuilder | SlashCommandSubcommandsOnlyBuilder;
   execute(interaction: ChatInputCommandInteraction, context: BotContext): Promise<void>;
 }
